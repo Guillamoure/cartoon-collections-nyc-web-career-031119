ab3c3cee25740b7a3cafe4b5a561663f5a3704b2
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-25 14:36:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each do |dwarf|
-    puts dwarf
+  array.each_with_index do |dwarf index|
+    number = index +1
+    puts "#{number}. #{dwarf}"
   end
 end
 
