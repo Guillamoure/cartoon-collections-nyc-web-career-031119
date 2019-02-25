276f54f4d3c18768868e31795c13ff06ce4aff6c
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-25 14:44:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |food|
-    food
+    food & cheese_types
+    if true
   end
 end
