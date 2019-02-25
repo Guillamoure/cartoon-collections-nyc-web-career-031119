18c9706b3ed8b119af1eac42a7120bf9cc5a066e
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-25 14:50:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |food|
+    place = 0
     cheese_types.include? food
     if true
       return food
