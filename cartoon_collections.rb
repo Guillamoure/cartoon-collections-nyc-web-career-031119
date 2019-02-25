2a869c08b40e982bc149947a0b75daff8e19f0bd
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-25 14:39:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,10 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  
+  array.map do |name|
+    shout = name.capitalize
+    name << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
