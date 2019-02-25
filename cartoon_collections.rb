88de55b61bf9806e6e89228654071caf1d9dbf5a
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-25 15:12:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,8 @@ def find_the_cheese(array)# code an argument here
     cheese_types.include? name
     if FALSE
     else
-      #cheese_types[4] == name
-      if TRUE
+      if cheese_types[4] == name
+      TRUE
         return "butt"
       else
         n += 1
